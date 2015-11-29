f03b2e2140541a6c189191980372d5f68cc66e93
Sun Robin <java.o.sun@gmail.com>
2015-11-29 16:03:45 +0800

    Tesst

--- GetTickets/Main.cs
+++ GetTickets/Main.cs
@@ -16,5 +16,18 @@ namespace GetTickets
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string domain = @"http://flight.qunar.com/";
+            string path = @"fuzzysearch/inspire/search/" +
+                            @"roundTripCity?callback=jQuery17205575992022641003_1448759871196&depCode=BJS" +
+                            @"&arr=%E4%B8%9C%E4%BA%AC(TYO)&arrType=1&interval=4%2C5&filter=%7B%7D&" +
+                            @"depDateRange =2016-01-01~2016-01-" +
+                            @"31%7C1%2C2016-02-01~2016-02-29%7C1%2C2016-03-01~2016-03-31%7C1&_=1448764182303";
+            
+            WebUtil webUtil = new WebUtil();
+            webUtil.GetResponse(domain, path);
+        }
     }
 }
--- GetTickets/WebUtil.cs
+++ GetTickets/WebUtil.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,28 @@ namespace GetTickets
     public class WebUtil
     {
         
-        public string GetResponse(string url)
+        public async void GetResponse(string domain, string path)
         {
-            return "";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(domain);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(path);
+                    response.EnsureSuccessStatusCode();    // Throw if not a success code.  
+
+                    string content = await response.Content.ReadAsStringAsync();
+                                      
+                    
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
 
     }
