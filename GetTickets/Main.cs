b64212657cefe6e6a24ac7dd4bd7f972efe8c562
SunRobin2015 <java.o.sun@gmail.com>
2015-11-30 15:08:24 +0800

    Add the function

--- GetTickets/Main.cs
+++ GetTickets/Main.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Timer = System.Threading.Timer;
 
 namespace GetTickets
 {
     public partial class Main : Form
     {
+
         public Main()
         {
             InitializeComponent();
@@ -20,14 +24,35 @@ namespace GetTickets
         private void button1_Click(object sender, EventArgs e)
         {
             string domain = @"http://flight.qunar.com/";
+            string depDate = "2015-12-01~2015-12-31%7C1%2C2016-01-01~2016-01-31%7C1%2C2016-02-01~2016-02-29%7C1%2C2016-03-01~2016-03-31";
+            //string days = "3-15";
+            string days = "3%2C4%2C5%2C6%2C7%2C8";
             string path = @"fuzzysearch/inspire/search/" +
                             @"roundTripCity?callback=jQuery17205575992022641003_1448759871196&depCode=BJS" +
-                            @"&arr=%E4%B8%9C%E4%BA%AC(TYO)&arrType=1&interval=4%2C5&filter=%7B%7D&" +
-                            @"depDateRange =2016-01-01~2016-01-" +
-                            @"31%7C1%2C2016-02-01~2016-02-29%7C1%2C2016-03-01~2016-03-31%7C1&_=1448764182303";
+                            @"&arr=%E4%B8%9C%E4%BA%AC(TYO)&arrType=1&" +
+                            @"interval=" + days +
+                            @"&filter=%7B%7D&" +
+                            @"depDateRange=" + depDate + "%7C1&_=1448764182303";
+
             
-            WebUtil webUtil = new WebUtil();
-            webUtil.GetResponse(domain, path);
+
+
+            Start(domain, path);
         }
+
+        private async void Start(string domain, string path)
+        {
+            WebUtil webUtil = new WebUtil();
+            while (true)
+            {
+                await webUtil.GetResponse(new URL(domain, path));
+                Thread.Sleep(15 * 1000);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(@"Start a new circle" + DateTime.Now);
+            }
+        }
+
     }
 }
--- GetTickets/WebUtil.cs
+++ GetTickets/WebUtil.cs
@@ -4,29 +4,60 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using GetTickets.Properties;
+using Newtonsoft.Json.Linq;
 
 namespace GetTickets
 {
     public class WebUtil
     {
-        
-        public async void GetResponse(string domain, string path)
+
+        public async Task GetResponse(URL url)
         {
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri(domain);
+                client.BaseAddress = new Uri(url.Domain);
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 try
                 {
-                    HttpResponseMessage response = await client.GetAsync(path);
+                    HttpResponseMessage response = await client.GetAsync(url.Path);
                     response.EnsureSuccessStatusCode();    // Throw if not a success code.  
-
                     string content = await response.Content.ReadAsStringAsync();
-                                      
-                    
+
+                    int startIndex = content.IndexOf('{');
+                    int endIndex = content.Length - 2;
+                    content = content.Substring(startIndex, endIndex - startIndex + 1);
+
+                    JObject jObject = JObject.Parse(content);
+                    JObject maps = jObject["data"]["priceMap"] as JObject;
+                    List<Flight> list = new List<Flight>();
+                    foreach (var map in maps)
+                    {
+                        string name = map.Key;
+                        JToken m = map.Value;
+                        Flight f = new Flight()
+                        {
+                            Name = name,
+                            Price = Convert.ToInt32(m["prWithTax"])
+                        };
+                        list.Add(f);
+                    }
+
+                    var list2 = list.OrderBy(f => f.Price);
+                    foreach (var flight in list2)
+                    {
+                        if (flight.Price <= 1300)
+                        {
+                            MessageBox.Show("The ticket is coming!!!");
+                        }
+                        Console.WriteLine(flight.Name + Resources.WebUtil_GetResponse____ + flight.Price);
+                    }
+
                 }
                 catch (HttpRequestException e)
                 {
@@ -35,5 +66,26 @@ namespace GetTickets
             }
         }
 
+    }
+
+
+    public class Flight
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+
+    public class URL
+    {
+        public string Domain { get; set; }
+
+        public string Path { get; set; }
+
+        public URL(string domain, string path)
+        {
+            Domain = domain;
+            Path = path;
+        }
     }
 }
